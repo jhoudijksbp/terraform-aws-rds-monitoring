b41f0e371e6165a40cf6114f60f6c643f5b22e50
Jeffrey Houdijk <jhoudijk@schubergphilis.com>
2022-03-31 11:13:11 +0200

    latest version

--- rds_alarms.tf
+++ rds_alarms.tf
@@ -1,19 +1,62 @@
+//CPUUtilization
 resource "aws_cloudwatch_metric_alarm" "cpu_utilization_too_high" {
   for_each            = toset(var.rds_instance_ids)
   alarm_name          = "rds-${each.key}-highCPUUtilization"
+  alarm_actions       = [aws.aws_sns_topic.rds_monitoring]
+  alarm_description   = "Average database CPU utilization is too high."
   comparison_operator = "GreaterThanThreshold"
-  evaluation_periods  = 1
+  evaluation_periods  = var.evaluation_period
   metric_name         = "CPUUtilization"
   namespace           = "AWS/RDS"
-  period              = 600
+  ok_actions          = [aws.aws_sns_topic.rds_monitoring]
+  period              = var.statistic_period
   statistic           = "Average"
-  threshold           = 90
-  alarm_description   = "Average database CPU utilization is too high."
+  threshold           = var.cpu_utilization_too_high_threshold
   tags                = var.tags
-  #alarm_actions       = var.actions_alarm
-  #ok_actions          = var.actions_ok
 
   dimensions = {
     DBInstanceIdentifier = each.key
   }
 }
+
+// Disk Utilization
+resource "aws_cloudwatch_metric_alarm" "disk_queue_depth_too_high" {
+  for_each            = toset(var.rds_instance_ids)
+  alarm_name          = "rds-${each.key}-highDiskQueueDepth"
+  alarm_actions       = var.actions_alarm
+  alarm_description   = "Average database disk queue depth is too high, performance may be negatively impacted."
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = var.evaluation_period
+  metric_name         = "DiskQueueDepth"
+  namespace           = "AWS/RDS"
+  ok_actions          = var.actions_ok
+  period              = var.statistic_period
+  statistic           = "Average"
+  threshold           = var.disk_queue_depth_too_high_threshold
+  tags                = var.tags
+
+  dimensions = {
+    DBInstanceIdentifier = each.key
+  }
+}
+
+// Lag replication
+resource "aws_cloudwatch_metric_alarm" "replicalag_too_high" {
+  for_each            = toset(var.rds_instance_ids)
+  alarm_name          = "rds-${each.key}-replicaLag"
+  alarm_actions       = var.actions_alarm
+  alarm_description   = "Average database replicaLagis too high, readers are too far behind master"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = var.evaluation_period
+  metric_name         = "DiskQueueDepth"
+  namespace           = "AWS/RDS"
+  ok_actions          = var.actions_ok
+  period              = var.statistic_period
+  statistic           = "Average"
+  threshold           = var.replicalag_threshold
+  tags                = var.tags
+
+  dimensions = {
+    DBInstanceIdentifier = each.key
+  }
+  
--- variables.tf
+++ variables.tf
@@ -1,13 +1,37 @@
+variable "cpu_utilization_too_high_threshold" {
+  type        = string
+  default     = "90"
+  description = "Alarm threshold for the 'highCPUUtilization' alarm"
+}
+
+variable "disk_queue_depth_too_high_threshold" {
+  type        = string
+  default     = "64"
+  description = "Alarm threshold for the 'highDiskQueueDepth' alarm"
+}
+
 variable "email_endpoint" {
   type        = string
   default     = ""
   description = "Comma separated list of e-mail addresses"
 }
 
+variable "evaluation_period" {
+  type        = string
+  default     = "5"
+  description = "The evaluation period over which to use when triggering alarms."
+}
+
 variable "kms_key_id" {
   type        = string
   default     = null
   description = "The KMS key ID used for SNS topic"
+}
+
+variable "replicalag_threshold" {
+  type        = string
+  default     = "300000" # 5 minutes
+  description = "Alarm threshold in ms for the 'replicaLag' alarm"
 }
 
 variable "rds_instance_ids" {
@@ -19,6 +43,12 @@ variable "send_email_alerts" {
   type        = bool
   default     = false
   description = "If this module needs to sent e-mail alerts"
+}
+
+variable "statistic_period" {
+  type        = string
+  default     = "60"
+  description = "The number of seconds that make each statistic period."
 }
 
 variable "tags" {
